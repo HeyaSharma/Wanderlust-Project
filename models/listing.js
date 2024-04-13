9902e7d7572ea2368d5bd76b7d6f0c5f6ed6f68c
Heya Sharma <heyaasharma@gmail.com>
2024-04-13 21:58:10 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+      title: {
+        type: String,
+        required: true,
+      },
+      description:String,
+      image:{
+        filename: String,
+        url:{
+        type: String,
+        default: 
+            "https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ",
+        set: (v) => 
+          v === "" 
+          ? "https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ" 
+          : v ,
+      }},
+    
+      price:Number,
+      location:String,
+      country:String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
