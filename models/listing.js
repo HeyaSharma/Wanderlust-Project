a5c985299610de877bf7c612dc39d0b9b935f47a
Heya Sharma <heyaasharma@gmail.com>
2024-04-14 19:35:11 +0530

    changes made
    changes

--- models/listing.js
+++ models/listing.js
@@ -12,11 +12,10 @@ const listingSchema = new Schema ({
         url:{
         type: String,
         default: 
-            "https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ",
+            "https://images.unsplash.com/photo-1712928247899-2932f4c7dea3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) => 
-          v === "" 
-          ? "https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ" 
-          : v ,
+          v === "https://images.unsplash.com/photo-1712928247899-2932f4c7dea3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          ? "https://images.unsplash.com/photo-1712928247899-2932f4c7dea3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v ,
       }},
     
       price:Number,
