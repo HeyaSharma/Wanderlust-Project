b992d1a98dfc32a229c752b6afa023a301b5df3b
Heya Sharma <heyaasharma@gmail.com>
2024-04-17 19:28:31 +0530

    changes made

--- public/js/map.js
+++ public/js/map.js
@@ -11,7 +11,7 @@ const marker = new mapboxgl.Marker({ color: "red"})
         .setLngLat(listing.geometry.coordinates)
         .setPopup( new mapboxgl.Popup({offset: 25})
         .setHTML(
-            '<h4>${listing.location} </h4><p>Exact Location will be provided after booking</p>'
+            `<h4>${listing.location} </h4><p>Exact Location will be provided after booking</p>'`
         )
     )
         .addTo(map);
